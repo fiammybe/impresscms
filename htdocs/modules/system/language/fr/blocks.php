e45233ea794c8845584fa30b43156e5b33835595
David Janssens <fiammybe@gmail.com>
2025-06-16 12:01:26 +0200

    New translations blocks.php (French)

---
+++ htdocs/modules/system/language/fr/blocks.php
@@ -0,0 +1,62 @@
+<?php
+// $Id: blocks.php 9349 2009-09-06 19:25:40Z Phoenyx $
+// Blocks
+define("_MB_SYSTEM_ADMENU","Administration");
+define("_MB_SYSTEM_RNOW","Inscrivez-vous!");
+define("_MB_SYSTEM_LPASS","Mot de passe perdu ?");
+define("_MB_SYSTEM_SEARCH","Recherche");
+define("_MB_SYSTEM_ADVS","Recherche avanc&eacute;e");
+define("_MB_SYSTEM_VACNT","Voir mon compte");
+define("_MB_SYSTEM_EACNT","Editer mon compte");
+// RMV-NOTIFY
+define("_MB_SYSTEM_NOTIF", "Notifications");
+define("_MB_SYSTEM_LOUT","D&eacute;connexion");
+define("_MB_SYSTEM_INBOX","Mes messages");
+define("_MB_SYSTEM_HOME","Accueil"); // link to home page in main menu block
+define("_MB_SYSTEM_RECO","Recommandez-nous");
+define("_MB_SYSTEM_PWWIDTH","Largeur de la fen&ecirc;tre Pop-Up");
+define("_MB_SYSTEM_PWHEIGHT","Hauteur de la fen&ecirc;tre Pop-Up");
+define("_MB_SYSTEM_LOGO","Fichier image du logo dans le r&eacute;pertoire %s");  // %s is your root image directory name
+
+define("_MB_SYSTEM_SADMIN","Afficher les groupes admin");
+
+define("_MB_SYSTEM_DISPLAY","Afficher %s membres");
+define("_MB_SYSTEM_DISPLAYA","Afficher les avatars des membres");
+define("_MB_SYSTEM_NODISPGR","Ne pas afficher les utilisateurs dont le classement est:");
+
+define("_MB_SYSTEM_DISPLAYC","Afficher %s commentaires");
+define("_MB_SYSTEM_SECURE", "Connexion s&eacute;curis&eacute;e");
+
+define("_MB_SYSTEM_NUMTHEME", "%s th&egrave;mes");
+define("_MB_SYSTEM_THSHOW", "Afficher une capture d'&eacute;cran");
+define("_MB_SYSTEM_THWIDTH", "Largeur de la capture d'&eacute;cran");
+define('_MB_SYSTEM_REMEMBERME', 'se souvenir de moi');
+define("_MB_SYSTEM_PRIVPOLICY", "Politique de confidentialit&eacute;");
+
+/*
+ * Added in 1.2
+ */
+
+define("_MB_SYSTEM_TOTAL_USERS", "Nombre totale d' utilisateurs");
+define("_MB_SYSTEM_ACT_USERS", "Utilisateurs actifs");
+define("_MB_SYSTEM_INACT_USERS", "Utilisateurs inactifs");
+define("_MB_SYSTEM_DISPLAYTOT","Totaal aantal leden informatie weergeven?");
+// waiting content
+define("_MB_SYSTEM_NOWAITING_DISPLAY","Montrer toujours");
+define("_MB_SYSTEM_SQL_CACHE","Cache SQL");
+// Social bookmarking
+define("_MB_SYSTEM_SOCIAL_PROVIDER_SELECT", "Choisir les providers de favoris");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_BOOKMARK", "bookmark at: ");
+
+/*
+ * Updated in 2.0 RC2
+ */
+
+define("_MB_SYSTEM_SOCIAL_PROVIDER_0", "Twitter");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_1", "Facebook");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_2", "Delicious");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_3", "Ask");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_4", "Digg");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_5", "Reddit");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_6", "Yahoo");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_7", "Diigo");
