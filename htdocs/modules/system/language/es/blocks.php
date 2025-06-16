adaae4547edcf7943acb6a1e3e291c074cf0efdc
David Janssens <fiammybe@gmail.com>
2025-06-16 12:01:27 +0200

    New translations blocks.php (Spanish)

---
+++ htdocs/modules/system/language/es/blocks.php
@@ -0,0 +1,62 @@
+<?php
+// $Id: blocks.php 9349 2009-09-06 19:25:40Z Phoenyx $
+// Blocks
+define("_MB_SYSTEM_ADMENU","Administration Menu");
+define("_MB_SYSTEM_RNOW","Register now!");
+define("_MB_SYSTEM_LPASS","Lost Password?");
+define("_MB_SYSTEM_SEARCH","Buscar");
+define("_MB_SYSTEM_ADVS","Búsqueda avanzada");
+define("_MB_SYSTEM_VACNT","View Account");
+define("_MB_SYSTEM_EACNT","Edit Account");
+// RMV-NOTIFY
+define("_MB_SYSTEM_NOTIF", "Notifications");
+define("_MB_SYSTEM_LOUT","Salir");
+define("_MB_SYSTEM_INBOX","Inbox");
+define("_MB_SYSTEM_HOME","Home"); // link to home page in main menu block
+define("_MB_SYSTEM_RECO","Recommend Us");
+define("_MB_SYSTEM_PWWIDTH","Pop-Up Window Width");
+define("_MB_SYSTEM_PWHEIGHT","Pop-Up Window Height");
+define("_MB_SYSTEM_LOGO","Logo image file under %s directory");  // %s is your root image directory name
+
+define("_MB_SYSTEM_SADMIN","Show admin groups");
+
+define("_MB_SYSTEM_DISPLAY","Display %s members");
+define("_MB_SYSTEM_DISPLAYA","Display member avatars");
+define("_MB_SYSTEM_NODISPGR","Do not display users whose rank is:");
+
+define("_MB_SYSTEM_DISPLAYC","Display %s comments");
+define("_MB_SYSTEM_SECURE", "Secure Login");
+
+define("_MB_SYSTEM_NUMTHEME", "%s themes");
+define("_MB_SYSTEM_THSHOW", "Display screenshot image");
+define("_MB_SYSTEM_THWIDTH", "Screenshot image width");
+define('_MB_SYSTEM_REMEMBERME', 'Remember me');
+define("_MB_SYSTEM_PRIVPOLICY", "Privacy Policy");
+
+/*
+ * Added in 1.2
+ */
+
+define("_MB_SYSTEM_TOTAL_USERS", "Total Users");
+define("_MB_SYSTEM_ACT_USERS", "Active Users");
+define("_MB_SYSTEM_INACT_USERS", "Inactive Users");
+define("_MB_SYSTEM_DISPLAYTOT","Display total members information?");
+// waiting content
+define("_MB_SYSTEM_NOWAITING_DISPLAY","Always displayed");
+define("_MB_SYSTEM_SQL_CACHE","SQL cache");
+// Social bookmarking
+define("_MB_SYSTEM_SOCIAL_PROVIDER_SELECT", "Select social bookmark providers to show");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_BOOKMARK", "bookmark at: ");
+
+/*
+ * Updated in 2.0 RC2
+ */
+
+define("_MB_SYSTEM_SOCIAL_PROVIDER_0", "Twitter");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_1", "Facebook");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_2", "Del.icio.us");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_3", "Ask");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_4", "Digg");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_5", "Reddit");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_6", "Yahoo");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_7", "Diigo");
