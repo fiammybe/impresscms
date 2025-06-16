caf4eb262d2e13f04ba73fc3e0efad95f3707e2f
David Janssens <fiammybe@gmail.com>
2025-06-16 11:49:24 +0200

    New translations install2.php (Lithuanian)

---
+++ htdocs/install/language/lt/install2.php
@@ -0,0 +1,84 @@
+<?php
+// $Id: install2.php 9920 2010-03-27 17:46:23Z skenow $
+define("_INSTALL_WEBMASTER","Webmasteriai");
+define("_INSTALL_WEBMASTERD","Šios svetainės webmasteriai");
+define("_INSTALL_REGUSERS","Registruoti vartotojai");
+define("_INSTALL_REGUSERSD","Registruotų vartotojų grupė");
+define("_INSTALL_ANONUSERS","Anoniminiai vartotojai");
+define("_INSTALL_ANONUSERSD","Anoniminių vartotojų grupė");
+
+define("_INSTALL_L165","Svetainė šiuo metu uždaryta dėl techninio priežiūros. Prašome užsukti vėliau.");
+
+define("_INSTALL_ANON","Anonimas");
+
+define("_INSTALL_DISCLMR", "Nors šios svetainės administratoriai ir moderatoriai bandys kuo greičiau pašalinti arba redaguoti bet kokią bendrai nepriimtiną medžiagą, nepavyks peržiūrėti kiekvieno pranešimo. Todėl jūs pripažįstate, kad visi šioje svetainėje pateikti įrašai atspindi autoriaus, o ne administratorių, moderatorių ar webmasterio (išskyrus šių asmenų įrašus) nuomones ir todėl jie nebus laikomi atsakingais.
+
+Jūs sutinkate nepasidalinti jokių įžeidžiančių, nešvarių, vulgarių, paniekinamų, neapčiuopiamų, grėsmingų, seksualinio pobūdžio ar bet kokios kitos medžiagos, kuri gali pažeisti bet kurį taikomą įstatymą. Padarydami taip, galite būti iškart ir amžinai užblokuoti (ir jūsų paslaugų teikėjas bus informuotas). Visų įrašų IP adresai yra registruojami, kad būtų užtikrinamas šių sąlygų laikymasis. Vienam vartotojui kurti keletas paskyrų yra draudžiama. Jūs sutinkate, kad šios svetainės webmasteris, administratorius ir moderatoriai turi teisę pašalinti, redaguoti, perkelti arba uždaryti bet kokią temą bet kuriuo metu, jei jie tai laiko būtina. Kaip vartotojas jūs sutinkate, kad visi aukščiau įvesti duomenys bus saugomi duomenų bazėje. Nors ši informacija nebus atskleista jokiai trečiai šaliai be jūsų sutikimo, webmasteris, administratorius ir moderatoriai negali būti laikomi atsakingais už bet kokį įsilaužimo bandymą, kuris gali lėmuoti duomenų pažeidimą.
+
+Ši svetainės sistema naudoja slapukus, kad saugotų informaciją jūsų lokalioje kompiuteryje. Šie slapukai neįtraukia jokios aukščiau įvestos informacijos, jie skirtas tik jūsų naršymo patogumui. El. pašto adresas naudojamas tik jūsų registracijos duomenų ir slaptažodžio patvirtinimui (ir naujų slaptažodžių siuntimui, jei pamiršote dabartinį).
+
+Paspaudę „Registruotis“ žemiau, jūs sutinkate laikytis šių sąlygų.");
+
+define("_INSTALL_PRIVPOLICY", "<p>Ši privatumo politika nurodo, kaip {X_SITENAME} naudoja ir saugo bet kokią informaciją, kurią jūs pateikiate naudodami šią svetainę. {X_SITENAME} įsipareigoję užtikrinti, kad jūsų privatumas būtų saugomas. Jei mes prašysime jūsų pateikti tam tikrą informaciją, pagal kurią galima identifikuoti, naudojant šią svetainę, galite būti įsitikinę, kad ji bus naudojama tik atitinkamai šiai privatumo pareiškimui. {X_SITENAME} gali keisti šią politiką nuo laikų atnaujinant šį puslapį. Jums reikia peržiūrėti šį puslapį nuo laikų iki laiko, kad įsitikintumėte, jog esate patenkinti bet kokiais pakeitimais.
+</p><p>
+Ši politika įsigalioja nuo [data].
+</p>
+<h2>Ką mes renkame</h2>
+<p>
+Mes gali surinkti šią informaciją:
+<ul>
+<li>vardas ir pareigos</li>
+<li>kontaktinė informacija, įskaitant el. pašto adresą</li>
+<li>demografinė informacija, tokia kaip pašto kodas, pageidavimai ir interesai</li>
+<li>kita informacija, susijusi su klientų apklausomis ir/ar pasiūlymais</li></ul>
+</p>
+<h2>Ką mes darome su surinkta informacija</h2>
+<p>
+Mums reikia šios informacijos, kad suprastume jūsų poreikius ir teiktume jums geresnę paslaugą, ypač dėl šių priežasčių:
+<ul>
+<li>Vidaus įrašų tvarkymas.</li>
+<li>Mes gali naudoti informaciją, kad patobulintume mūsų produktus ir paslaugas.</li>
+<li>Periodiškai mes gali siųsti jums reklaminius el. laiškus apie naujus produktus, specialius pasiūlymus ar kitą informaciją, kuri, mūsų nuomone, gali jums būti įdomi, naudojant el. pašto adresą, kurį jūs pateikėte.</li>
+<li>Kartais mes gali naudoti jūsų informaciją, kad su jumis susisiektume tyrimo tikslais. Mes gali su jumis susisiekti el. paštu.</li>
+<li>Mes gali naudoti informaciją, kad pritaikytume svetainę jūsų interesams.</li></ul>
+</p>
+<h2>Saugumas</h2>
+<p>
+Mes įsipareigojame užtikrinti, kad jūsų informacija būtų saugi. Siekiant išvengti neautorizuoto prieigos ar atskleidimo, mes įvedėme tinkamus fizinius, elektroninius ir valdymo procesus, kad saugotume ir apsaugotume informaciją, kurią renkame internetu.
+</p>
+<h2>Kaip mes naudojame slapukus</h2>
+<p>
+Slapukas yra mažas failas, kuris prašo leidimo būti įkeltas į jūsų kompiuterio kietąjį diską. Kai jūs sutinkate, failas yra pridėtas ir slapukas padeda analizuoti interneto eismą arba pranešti jums, kai aplankote konkrečią svetainę. Slapukai leidžia internetinėms programoms atsakyti į jūsų kaip asmenį. Internetinė programa gali pritaikyti savo veikimą jūsų poreikiams, pageidavimams ir nepageidavimams, surinkdama ir prisiminėdama informaciją apie jūsų nuostatas.
+</p><p>
+Mes naudojame eismo žurnalo slapukus, kad identifikuotume, kurios puslapiai yra naudojamos, ir kad jūsų autentifikuotume kaip registruotą narį. Tai padeda mums analizuoti duomenis apie internetinio puslapio eismą ir patobulinti mūsų svetainę, kad ji atitiktų klientų poreikius. Mes naudojame šią informaciją tik statistinės analizės tikslais, o po to duomenys yra pašalinami iš sistemos. Visų pirma, slapukai padeda mums teikti jums geresnę svetainę, leidžiant mums stebėti, kurie puslapiai jums yra naudingi, o kurie ne. Slapukas niekaip neleidžia mums pasiekti jūsų kompiuterio ar bet kokios informacijos apie jūsų, išskyrus tuos duomenis, kuriuos norite su mumis pasidalinti.
+</p><p>
+Jūs galite pasirinkti priimti arba atmesti slapukus. Dauguma interneto naršyklių automatiškai priima slapukus, bet paprastai galite pakeisti savo naršyklės nustatymus, kad atmestumėte slapukus, jei norite. Tai gali jums sutrukdyti pilnai naudotis svetaine, įskaitant registraciją ir prisijungimą.
+</p>
+<h2>Nuorodos į kitų svetainės</h2>
+<p>
+Mūsų svetainėje gali būti nuorodų, leidžiančių jums lengvai lankytis kitose jums įdomiomis svetainėmis. Tačiau, kai jūs naudojatės šiomis nuorodomis, kad paliktumėte mūsų svetainę, turėtumėte įsitikinti, kad mes neturime jokios kontroliuojamosios galios kitoje svetainėje. Todėl mes negalime būti atsakingi už bet kokios informacijos, kurią pateikiate lankydamiesi tokioje svetainėje, saugumą ir privatumą, ir tokios svetainės nėra reguliuojamos šio privatumo pareiškimo. Jums reikia būti atsargiam ir pažiūrėti į privatumo pareiškimą, taikomą minėtai svetainei.
+</p>
+<h2>Jūsų asmeninės informacijos valdymas</h2>
+<p>
+Jūs galite apriboti savo asmeninės informacijos rinkimą ar naudojimą šiais būdais:
+<ul>
+<li>kai jums prašoma užpildyti formą svetainėje, ieškokite langelio, kurį galėtumėte pažymėti, kad norenate, kad jūsų informacija nebūtų naudojama tiesioginės rinkodaros tikslais</li>
+<li>jei anksčiau sutikote, kad mes naudotume jūsų asmeninę informaciją tiesioginės rinkodaros tikslais, galite pakeisti savo nuomonę bet kuriuo metu parašydami arba el. paštu mums į [el. pašto adresas]</li></ul>
+</p><p>
+Mes neparduosime, nepardavysime ar neleisime jūsų asmeninės informacijos trečioms šalims, išskyrus atvejus, kai turėsime jūsų leidimo arba tai reikalaus įstatymas. Mes gali naudoti jūsų asmeninę informaciją, kad siųstume jums reklaminius pranešimus apie trečias šalis, kurios, mūsų nuomone, gali jums būti įdomios, jei jūs mums pasakysite, kad norite, kad tai būtų padaryta. Jūs galite prašyti mūsų turimos apie jūsų asmeninės informacijos pagal 1998 m. Duomenų apsaugos įstatymą. Už tai bus mokamas nedidelis mokestis. Jei norite gauti informacijos, kurią turime apie jūsų, kopiją, prašome rašyti į [adresas].
+</p><p>
+Jei manote, kad bet kuri informacija apie jūsų, kurią mes saugome, yra neteisinga arba nepilna, prašome kuo greičiau parašyti arba el. paštu mums į aukščiau nurodytą adresą. Mes kuo greičiau pataisysime bet kurią neteisingą informaciją.
+</p>");
+define("_WELCOME_MSG_CONTENT", "Sveiki atvykę {UNAME},
+
+Jūsų paskyra buvo sėkmingai aktyvuota {X_SITENAME}. Kaip mūsų svetainės narys, jūs naudosis visomis funkcijomis, skirtomis registruotiesiems nariams!
+
+Dar kartą sveiki atvykę į mūsų svetainę. Lankykitės dažnai!
+
+Jei jūs nesiregistravote į mūsų svetainę, prašome susisiekti su mumis šiuo adresu {X_ADMINMAIL}, ir mes ištaisysime situaciją.
+
+-----------
+Su pagarbė,
+{X_SITENAME}
+
+{X_SITEURL}");
