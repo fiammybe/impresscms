67328c782b9fb5d26155baf780c4bdd16ef84d08
David Janssens <fiammybe@gmail.com>
2025-06-16 12:08:58 +0200

    New translations mimetype.php (German)

---
+++ htdocs/modules/system/language/de/admin/mimetype.php
@@ -0,0 +1,30 @@
+<?php
+
+define('_CO_ICMS_MIMETYPES','Mime Typen');
+define('_CO_ICMS_MIMETYPES_DSC', 'Hier finden Sie eine Liste der AdSense-Anzeigen im System.');
+define('_CO_ICMS_MIMETYPE', 'Mime-Typ');
+define('_CO_ICMS_MIMETYPE_CREATE', 'Mime-Typ erstellen');
+define('_CO_ICMS_MIMETYPE_CREATE_INFO', 'Füllen Sie einfach dieses Formular aus, um ein eigenes Schlagwörter hinzuzufügen.');
+define('_CO_ICMS_MIMETYPE_DELETE_CONFIRM', 'Möchten Sie diese Adsense-Anzeige wirklich aus dem System löschen?');
+define('_CO_ICMS_MIMETYPE_EDIT', 'Mime-Typ bearbeiten');
+define('_CO_ICMS_MIMETYPE_EDIT_INFO', 'Verwenden Sie dieses Formular, um die Informationen dieses Mime-Typs zu bearbeiten.');
+define('_CO_ICMS_MIMETYPE_INFO', 'Mime Typ-Informationen');
+define('_CO_ICMS_MIMETYPE_NOT_FOUND', 'Der ausgewählte Mime-Typ wurde nicht gefunden.');
+
+define('_CO_ICMS_MIMETYPE_CREATED', 'Der Mime-Typ wurde erfolgreich erstellt.');
+define('_CO_ICMS_MIMETYPE_MODIFIED', 'Der Mime-Typ wurde erfolgreich geändert.');
+
+define('_CO_ICMS_MIMETYPE_NAME', 'Typ der Anwendung');
+define('_CO_ICMS_MIMETYPE_NAME_DSC', "Medientypen/Name<div style='padding-top: 8px;'><span style='font-weight: normal;'>Geben Sie ein Medientyp mit der Anwendung zur Dateiendung ein.</span></div>");
+define('_CO_ICMS_MIMETYPE_EXTENSION', 'Dateiendung');
+define('_CO_ICMS_MIMETYPE_EXTENSION_DSC', '');
+define('_CO_ICMS_MIMETYPE_TYPES', "MIME-Type");
+define('_CO_ICMS_MIMETYPE_TYPES_DSC', "MIME-Types<div style='padding-top: 8px;'><span style='font-weight: normal;'>Geben Sie eine oder mehrere Dateiendungen ein, dabei muss jede Dateiendung wird durch ein Leerzeichen getrennt.</span></div>");
+
+define('_CO_ICMS_MIMETYPE_PERMISSION_VIEW', 'Berechtigung anzeigen');
+define('_CO_ICMS_MIMETYPE_PERMISSION_VIEW_DSC', 'Nur die ausgewählten Gruppen können diesen Mimetype verwenden');
+
+define('_CO_ICMS_MIMETYPES_EXPLAIN_TITLE', 'Was sind Mime Typen?');
+define('_CO_ICMS_MIMETYPES_EXPLAIN', 'Mime Typen sind Erweiterungen zum Hochladen von Dateien auf einen Webserver, dieser Abschnitt hilft Ihnen alle erlaubten mimetypes für Ihre Benutzer zu kontrollieren! Vollständige Dokumentation dieser Funktion finden Sie hier: <a rel="external" href="https://www.impresscms.org/modules/simplywiki/index.php?page=MimeTypes">Mime Types</a>.');
+define('_CO_ICMS_MIMETYPE_DIRNAME', 'Wählen Sie Module aus, die diesen Mimetype verwenden dürfen.');
+?>
