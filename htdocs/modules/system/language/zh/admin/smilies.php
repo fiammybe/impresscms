d7b857dac48153cb45cf89e295bfdbd25982e2e3
David Janssens <fiammybe@gmail.com>
2025-06-16 12:11:28 +0200

    New translations smilies.php (Chinese Simplified)

---
+++ htdocs/modules/system/language/zh/admin/smilies.php
@@ -0,0 +1,22 @@
+<?php
+// $Id: smilies.php 8535 2009-04-11 10:08:35Z icmsunderdog $
+//%%%%%%	Admin Module Name  Smilies 	%%%%%
+define('_AM_DBUPDATED','数据库更新成功!');
+
+define('_AM_SMILESCONTROL','Smilies Control');
+define('_AM_CODE','代码');
+define('_AM_SMILIE','Smilie');
+define('_AM_ACTION','Action');
+define('_AM_EDIT','编辑');
+define('_AM_DEL','移除');
+define('_AM_CNRFTSD','Could not retrieve from the smilies database.');
+define('_AM_ADDSMILE','Add a Smilie');
+define('_AM_EDITSMILE','Edit a Smilie');
+define('_AM_SMILECODE','Smilie Code:');
+define('_AM_SMILEURL','Image URL:');
+define('_AM_SMILEEMOTION','Description:');
+define('_AM_ADD','添加');
+define('_AM_SAVE','Save');
+define('_AM_WAYSYWTDTS','WARNING: Are you sure you want to delete this Smile?');
+define('_AM_DISPLAYF', 'Display in form?');
+?>
