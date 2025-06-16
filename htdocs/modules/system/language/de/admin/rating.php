8b597b463f796b72ed05d55c86ad5bdea0dc3aa4
David Janssens <fiammybe@gmail.com>
2025-06-16 12:10:48 +0200

    New translations rating.php (German)

---
+++ htdocs/modules/system/language/de/admin/rating.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * rating definations
+ *
+ * @copyright http://www.impresscms.org/ The ImpressCMS Project
+ * @license LICENSE.txt
+ * @package ratings
+ * @author marcan <macrcan@impresscms.org>
+ */
+if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
+
+define('_CO_ICMS_RATING_RATE_IT', 'Bewerten !');
+define('_CO_ICMS_RATING_DUPLICATE_ENTRY', 'Entschuldigung! Sie haben dies bereits bewertet! Sie können nicht zweimal bewerten.');
+define('_CO_ICMS_RATING_ERROR', 'An error occurred, your rating could not be stored.');
+define('_CO_ICMS_RATING_SUCCESS', 'Ihre Bewertung wurde gespeichert. Vielen Dank für Ihren Beitrag !');
+define('_CO_ICMS_RATING_RATE_THIS', 'Was this useful ? (1 being <i>Not useful</i> and 5 being <i>Very useful</i>)');
+define('_CO_ICMS_RATING_ALREADY_RATED', 'Ihre Bewertung: ');
+define('_CO_ICMS_RATING_VOTERS_TOTAL', 'Bewertung gesamt:');
+define('_CO_ICMS_RATING_AVERAGE', 'Durchschnitt: ');
+define('_CO_ICMS_RATINGS', 'Bewertungen');
+define('_CO_ICMS_RATINGS_DSC', 'Hier ist eine Liste der Bewertungen aus dem System.');
+define('_CO_ICMS_RATING', 'Bewertungen');
+define('_CO_ICMS_RATINGS_CREATE', 'Bewertung hinzufügen');
+define('_CO_ICMS_RATING_CREATE', 'Bewertung hinzufügen');
+define('_CO_ICMS_RATINGS_CREATE_INFO', 'Füllen Sie einfach dieses Formular aus, um eine Bewertung hinzuzufügen.');
+define('_CO_ICMS_RATING_DELETE_CONFIRM', 'Möchten Sie diese Bewertung wirklich aus dem System löschen?');
+define('_CO_ICMS_RATINGS_EDIT', 'Bewertung bearbeiten');
+define('_CO_ICMS_RATINGS_EDIT_INFO', 'Benutzen Sie dieses Formular, um die Informationen zu dieser Bewertung zu ändern.');
+define('_CO_ICMS_RATINGS_INFO', 'Bewertungsinformation');
+define('_CO_ICMS_RATING_NOT_FOUND', 'Die ausgewählte Bewertung wurde nicht gefunden.');
+define('_CO_ICMS_RATINGS_CREATED', 'Die Bewertung wurde erfolgreich erstellt.');
+define('_CO_ICMS_RATINGS_MODIFIED', 'Die Bewertung wurde erfolgreich geändert.');
+define('_CO_ICMS_RATING_EXPLAIN_TITLE', 'Was sind Bewertungen?');
+define('_CO_ICMS_RATING_EXPLAIN', 'Ratings is a new feature being added ImpressCMS. With using this tool, you will be able to add a new rating method to your modules, and control the results through this section! Documentation for this feature can be found here: <a rel="external" href="https://www.impresscms.org/modules/simplywiki/index.php?page=Ratings">Rating Features</a>. Let us know how you would like to see this developed');
