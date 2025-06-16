1e03913f6029897418fb8f8817e9590fa61c0ba3
David Janssens <fiammybe@gmail.com>
2025-06-16 12:04:34 +0200

    New translations avatars.php (German)

---
+++ htdocs/modules/system/language/de/admin/avatars.php
@@ -0,0 +1,14 @@
+<?php
+// $Id: avatars.php 11285 2011-06-23 16:37:07Z phoenyx $
+//%%%%%% Avatar Manager %%%%%
+define('_MD_AVATARMAN','Avatar-Verwaltung');
+
+define('_MD_SYSAVATARS','System-Avatare');
+define('_MD_CSTAVATARS','Eigene Avatare');
+define('_MD_ADDAVT','Avatar hinzufügen');
+define('_MD_USERS','Benutzer, die diesen Avatar verwenden');
+define('_MD_RUDELIMG','Sind Sie sicher, dass Sie dieses Avatar-Bild löschen möchten?');
+define('_MD_FAILDEL', 'Fehler beim Löschen des Avatar %s aus der Datenbank');
+// added in 1.3
+define('_MD_RESTRICTIONS', 'Einschränkungen');
+?>
