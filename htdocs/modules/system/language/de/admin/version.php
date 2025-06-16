a5c5efa01cabb1b9e2bcd87e0504d44b595264ec
David Janssens <fiammybe@gmail.com>
2025-06-16 12:12:54 +0200

    New translations version.php (German)

---
+++ htdocs/modules/system/language/de/admin/version.php
@@ -0,0 +1,23 @@
+<?php
+// $Id: version.php 12227 2013-07-19 08:07:21Z fiammy $
+//%%%%%%	Admin Module Name  Version 	%%%%%
+if (!defined('_AM_DBUPDATED')) {define("_AM_DBUPDATED","Datenbank erfolgreich aktualisiert!");}
+define("_AM_VERSION_TITLE", 'ImpressCMS Versionsprüfung');
+define("_AM_VERSION_NO_UPDATE", 'Sie verwenden die neueste Version von ImpressCMS !');
+define("_AM_VERSION_UPDATE_NEEDED", 'Es gibt eine neue Version von ImpressCMS !');
+define("_AM_VERSION_MOREINFO", 'Klicken Sie auf den folgenden Link, um weitere Informationen über die neueste Version zu erhalten: ');
+
+define("_AM_VERSION_CHECK_RSSDATA_EMPTY", 'Es wurden keine Informationen zur Überprüfung einer aktualisierten Version zur Verfügung gestellt.');
+define("_AM_VERSION_CHANGELOG", 'Changelog');
+define("_AM_VERSION_WARNING", 'Warnung');
+define("_AM_VERSION_WARNING_NOT_A_FINAL", 'Bitte beachten Sie, dass Sie derzeit eine "Finale" Version von ImpressCMS verwenden und die vorgeschlagene aktualisierte Version keine "End"-Version ist. Alle anderen Releases als "Final" sollten nicht auf einer Produktionsumgebung verwendet werden. Wenn Ihre Installation von ImpressCMS auf einer Produktionsumgebung läuft, empfehlen wir Ihnen, auf eine endgültige Version zu warten. Weitere Informationen finden Sie hier: <a href="https://www.impresscms.org/modules/simplywiki/index.php?page=Final_release">hier</a>.');
+
+define("_AM_VERSION_YOUR_VERSION", 'Ihre ImpressCMS Version:');
+define("_AM_VERSION_LATEST_VERSION", 'Letzte ImpressCMS Version:');
+// Added in ImpressCMS 1.2
+define("_AM_VERSION_OP_SYSTEM", 'Betriebssystem des Server:');
+define("_AM_VERSION_MYSQL_SYSTEM", 'Ihre Datenbankversion:');
+define("_AM_VERSION_PHP_SYSTEM", 'Ihre PHP-Version:');
+define("_AM_VERSION_API_SYSTEM", 'Ihr API-Name:');
+define("_AM_VERSION_SYSTEM_INFO", 'Klicken Sie hier, um Ihre Systeminformationen anzuzeigen');
+?>
