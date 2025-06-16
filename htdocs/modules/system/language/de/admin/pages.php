a1cded04dcc69fd869db1435cd8eb9ae3dc8aca7
David Janssens <fiammybe@gmail.com>
2025-06-16 12:09:51 +0200

    New translations pages.php (German)

---
+++ htdocs/modules/system/language/de/admin/pages.php
@@ -0,0 +1,14 @@
+<?php
+######################## Added in 1.2 ###################################
+define('_AM_SYSTEM_PAGES_TITLE','Symlink Manager');
+
+define('_AM_SYSTEM_PAGES_CREATE','Neues Symlink hinzufügen');
+define('_AM_SYSTEM_PAGES_EDIT','Symlink bearbeiten');
+define('_AM_SYSTEM_PAGES_CREATED','Symlink erstellt!');
+define('_AM_SYSTEM_PAGES_MODIFIED','Symlink geändert!');
+
+define('_CO_SYSTEM_PAGES_PAGE_URL', 'Seiten-URL');
+define('_CO_SYSTEM_PAGES_PAGE_TITLE', 'Titel');
+define('_CO_SYSTEM_PAGES_PAGE_STATUS', 'Seitenstatus');
+define('_CO_SYSTEM_PAGES_PAGE_MODULEID','Module');
+?>
