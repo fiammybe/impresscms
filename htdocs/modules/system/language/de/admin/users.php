e6ecce9769dbe1caceed0c188e2281dcc30a5db2
David Janssens <fiammybe@gmail.com>
2025-06-16 12:12:29 +0200

    New translations users.php (German)

---
+++ htdocs/modules/system/language/de/admin/users.php
@@ -0,0 +1,70 @@
+<?php
+// $Id: users.php 11034 2011-02-19 20:12:41Z skenow $
+//%%%%%%	Admin Module Name  Users 	%%%%%
+if (!defined('_AM_DBUPDATED')) {define("_AM_DBUPDATED","Datenbank erfolgreich aktualisiert!");}
+
+define("_AM_AYSYWTDU","Bist du sicher, dass du Block '%s ' löschen möchtest?");
+define("_AM_BYTHIS","Dadurch werden alle Informationen für diesen Benutzer dauerhaft gelöscht.");
+define("_AM_YES","Ja");
+define("_AM_NO","Nein");
+define("_AM_YMCACF","Sie müssen alle Pflichtfelder ausfüllen");
+define("_AM_CNRNU","Konnte keinen neuen Benutzer registrieren.");
+define("_AM_EDEUSER","Benutzer bearbeiten/löschen");
+define("_AM_NICKNAME","Anzeigename");
+define("_AM_MODIFYUSER","Benutzer ändern");
+define("_AM_DELUSER","Benutzer löschen");
+define("_AM_GO","Go!");
+define("_AM_ADDUSER","Neuer Benutzer");
+define("_AM_NAME","Name");
+define("_AM_EMAIL","E-Mail");
+define("_AM_OPTION","Optionen");
+define("_AM_AVATAR","Profilbild");
+define("_AM_THEME","Themes");
+define("_AM_AOUTVTEAD","Erlaube anderen Benutzern, diese E-Mail-Adresse anzuzeigen");
+define("_AM_URL","URL");
+define("_AM_ICQ","ICQ");
+define("_AM_AIM","AIM");
+define("_AM_YIM","YIM");
+define("_AM_MSNM","MSNM");
+define("_AM_LOCATION","Standort");
+define("_AM_OCCUPATION","Beruf");
+define("_AM_INTEREST","Interessen");
+define("_AM_RANK","Rang");
+define("_AM_NSRA","Kein spezieller Rang zugewiesen");
+define("_AM_NSRID","Keine speziellen Ränge in der Datenbank");
+define("_AM_ACCESSLEV","Zugriffsebene");
+define("_AM_SIGNATURE","Signatur");
+define("_AM_PASSWORD","Passwort");
+define("_AM_INDICATECOF","* zeigt die Pflichtfelder an");
+define("_AM_NOTACTIVE","Dieser Benutzer wurde nicht aktiviert. Möchten Sie diesen Benutzer aktivieren?");
+define("_AM_UPDATEUSER","Benutzer aktualisieren");
+define("_AM_USERINFO","Benutzerinfo");
+define("_AM_USERID","Benutzer-ID");
+define("_AM_RETYPEPD","Passwort wiederholen");
+define("_AM_CHANGEONLY","(nur bei Änderungen)");
+define("_AM_USERPOST","Benutzerbeiträge");
+define("_AM_STORIES","Stories");
+define("_AM_COMMENTS","Kommentare");
+define("_AM_PTBBTSDIYT","Drücken Sie die Schaltfläche unten, um Daten zu synchronisieren, wenn Sie denken, dass die oben genannten Benutzerbeiträge Info nicht den aktuellen Status anzeigt");
+define("_AM_SYNCHRONIZE","Synchronisieren");
+define("_AM_USERDONEXIT","Benutzer existiert nicht!");
+define("_AM_STNPDNM","Entschuldigung, die neuen Passwörter stimmen nicht überein. Klicke zurück und versuche es erneut");
+define("_AM_CNGTCOM","Konnte nicht alle Kommentare abrufen");
+define("_AM_CNGTST","Konnte nicht alle Kommentare abrufen");
+define("_AM_CNUUSER","Konnte Benutzer nicht aktualisieren");
+define("_AM_CNGUSERID","Konnte Benutzer-IDS nicht abrufen");
+define("_AM_LIST","Liste");
+define("_AM_NOUSERS", "Keine Benutzer ausgewählt");
+
+define("_AM_CNRNU2","Der neue Benutzer konnte nicht zu Gruppen hinzugefügt werden: %s.");
+
+define('_AM_BADPWD','Falsches Passwort, Passwort darf keinen Benutzernamen enthalten.');
+
+######################## Added in 1.2 ###################################
+define("_AM_LOGINNAME","Loginname");
+define("_AM_REMOVED_USERS","Entfernte Benutzer");
+define("_AM_ADMIN_CAN_NOT_BE_DELETEED","Admin-Benutzer kann nicht gelöscht werden");
+define("_AM_USERS_DELETEED","löschen");
+define("_AM_COULD_NOT_DELETE","Konnte nicht gelöscht werden");
+define("_AM_A_USER_WITH_THIS_EMAIL_ADDRESS","Ein Benutzer mit dieser E-Mail-Adresse");
+define("_AM_ALREADY_EXISTS","ist bereits vorhanden");
