2c77834c4249cffc5aff1f3c8a97477960d47687
David Janssens <fiammybe@gmail.com>
2025-06-16 12:11:59 +0200

    New translations userrank.php (French)

---
+++ htdocs/modules/system/language/fr/admin/userrank.php
@@ -0,0 +1,32 @@
+<?php
+// $Id: userrank.php 9538 2009-11-13 18:59:32Z pesianstranger $
+
+######################## Added in 1.2 ###################################
+define('_CO_ICMS_USERRANKS','Paramètres des rangs d\'utilisateurs');
+define('_CO_ICMS_USERRANKS_DSC', 'Voici une liste des rangs d\'utilisateurs disponibles dans le système.');
+define('_CO_ICMS_USERRANK', 'Rang de l\'utilisateur');
+define('_CO_ICMS_USERRANKS_CREATE', 'Ajouter un rang utilisateur');
+define('_CO_ICMS_USERRANKS_CREATE_INFO', 'Remplissez simplement ce formulaire pour ajouter un rang d\'utilisateur.');
+define('_CO_ICMS_USERRANKS_DELETE_CONFIRM', 'Voulez-vous vraiment supprimer ce rang d\'utilisateur du système?');
+define('_CO_ICMS_USERRANKS_EDIT', 'Modifier un rang d\'utilisateur');
+define('_CO_ICMS_USERRANKS_EDIT_INFO', 'Utilisez ce formulaire pour modifier les informations de ce rang d\'utilisateur.');
+define('_CO_ICMS_USERRANK_INFO', 'Informations sur le rang de l\'utilisateur');
+define('_CO_ICMS_USERRANK_NOT_FOUND', 'Le rang d\'utilisateur sélectionné n\'a pas été trouvé.');
+
+define('_CO_ICMS_USERRANKS_CREATED', 'Le rang d\'utilisateur a été ajouté avec succès.');
+define('_CO_ICMS_USERRANKS_MODIFIED', 'Le rang d\'utilisateur a été modifié avec succès.');
+
+define('_CO_ICMS_USERRANK_RANK_SPECIAL','Rangs spéciaux');
+define('_CO_ICMS_USERRANK_RANK_SPECIAL_DSC','(Des rangs spéciaux peuvent être attribués aux utilisateurs indépendamment du nombre de contributions de l\'utilisateur)');
+define('_CO_ICMS_USERRANK_RANK_TITLE','Titre du rang d\'utilisateur');
+define('_CO_ICMS_USERRANK_RANK_TITLE_DSC','');
+define('_CO_ICMS_USERRANK_RANK_MIN','Contributions minimales');
+define('_CO_ICMS_USERRANK_RANK_MIN_DSC', '');
+define('_CO_ICMS_USERRANK_RANK_MAX','Contributions maximales');
+define('_CO_ICMS_USERRANK_RANK_MAX_DSC', '');
+define('_CO_ICMS_USERRANK_RANK_IMAGE','Image');
+define('_CO_ICMS_USERRANK_RANK_IMAGE_DSC', '');
+
+define('_CO_ICMS_USERRANK_EXPLAIN_TITLE', 'Que sont les rangs d\'utilisateurs ?');
+define('_CO_ICMS_USERRANK_EXPLAIN', 'Les rangs des utilisateurs sont des images, utilisées pour faire la différence entre les utilisateurs dans différents niveaux de votre site! La documentation complète de cette fonctionnalité peut être trouvée ici : <a rel="external" href="https://www.impresscms.org/modules/simplywiki/index.php?page=User_ranks">Rangs d\'utilisateurs</a>.');
+?>
