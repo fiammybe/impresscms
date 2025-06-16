cb1ca1a950cba6e333ed6121ad80c5134690e8b7
David Janssens <fiammybe@gmail.com>
2025-06-16 12:06:52 +0200

    New translations findusers.php (Japanese)

---
+++ htdocs/modules/system/language/ja/admin/findusers.php
@@ -0,0 +1,57 @@
+<?php
+// $Id: findusers.php 11344 2011-08-30 16:33:29Z phoenyx $
+//%%%%%%	File Name findusers.php 	%%%%%
+
+define("_AM_FINDUS","ユーザを見つける");
+define("_AM_AVATAR","アバター（似顔絵）");
+define("_AM_REALNAME","本名");
+define("_AM_REGDATE","登録日");
+define("_AM_EMAIL","メール");
+define("_AM_PM","PM");
+define("_AM_URL","URL");
+define("_AM_PREVIOUS","<< 前のデータ");
+define("_AM_NEXT","次のデータ >>");
+define("_AM_USERSFOUND","%s ユーザいらっしゃいます");
+
+define("_AM_ACTUS", "アクティブなユーザ数: %s");
+define("_AM_INACTUS", "アクティブじゃないユーザ数: %s");
+define("_AM_NOFOUND","ユーザが見つかりません");
+define("_AM_UNAME","ユーザ名");
+define("_AM_ICQ","ICQ番号");
+define("_AM_AIM","AIMハンドル");
+define("_AM_YIM","YIMハンドル");
+define("_AM_MSNM","MSNMハンドル");
+define("_AM_LOCATION","お住まい");
+define("_AM_OCCUPATION","ご職業");
+define("_AM_INTEREST","ご趣味");
+define("_AM_URLC","ホームページ");
+define("_AM_LASTLOGMORE","Last login is more than <span style='color:#ff0000;'>X</span> days ago");
+define("_AM_LASTLOGLESS","Last login is less than <span style='color:#ff0000;'>X</span> days ago");
+define("_AM_REGMORE","Joined date is more than <span style='color:#ff0000;'>X</span> days ago");
+define("_AM_REGLESS","Joined date is less than <span style='color:#ff0000;'>X</span> days ago");
+define("_AM_POSTSMORE","Number of Posts is greater than <span style='color:#ff0000;'>X</span>");
+define("_AM_POSTSLESS","Number of Posts is less than <span style='color:#ff0000;'>X</span>");
+define("_AM_SORT","Sort by");
+define("_AM_ORDER","Order");
+define("_AM_LASTLOGIN","前回ログイン");
+define("_AM_POSTS","投稿した数");
+define("_AM_ASC","Ascending order");
+define("_AM_DESC","Descending order");
+define("_AM_LIMIT","1ページあたりのユーザ数");
+define("_AM_RESULTS", "検索結果");
+define("_AM_SHOWMAILOK", "Accept mail");
+define("_AM_MAILOK","ユーザーメール可能");
+define("_AM_MAILNG","ユーザーメール不可能");
+define("_AM_SHOWTYPE", "アクティブ");
+define("_AM_ACTIVE","Only active users");
+define("_AM_INACTIVE","Only inactive users");
+define("_AM_BOTH", "All users");
+define("_AM_SENDMAIL", "Send mail");
+define("_AM_ADD2GROUP", "Add users to %s group");
+define("_AM_GROUPS", "Groups");
+
+######################## Added in 1.2 ###################################
+define("_AM_LOGINNAME","ログイン名");
+
+######################## Added in 1.3 ###################################
+define("_AM_ACTIONS","Actions");
