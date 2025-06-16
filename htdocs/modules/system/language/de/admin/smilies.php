ab4e2409e4062ed48652e5d357ec4af59581089c
David Janssens <fiammybe@gmail.com>
2025-06-16 12:11:13 +0200

    New translations smilies.php (German)

---
+++ htdocs/modules/system/language/de/admin/smilies.php
@@ -0,0 +1,22 @@
+<?php
+// $Id: smilies.php 8535 2009-04-11 10:08:35Z icmsunderdog $
+//%%%%%%	Admin Module Name  Smilies 	%%%%%
+define('_AM_DBUPDATED','Datenbank erfolgreich aktualisiert!');
+
+define('_AM_SMILESCONTROL','Smilies Kontrolle');
+define('_AM_CODE','Code');
+define('_AM_SMILIE','Smiley');
+define('_AM_ACTION','Aktion');
+define('_AM_EDIT','Bearbeiten');
+define('_AM_DEL','Löschen');
+define('_AM_CNRFTSD','Konnte von der Smilies-Datenbank nicht abrufen.');
+define('_AM_ADDSMILE','Smiley hinzufügen');
+define('_AM_EDITSMILE','Smiley bearbeiten');
+define('_AM_SMILECODE','Smiley-Code:');
+define('_AM_SMILEURL','Bild-URL:');
+define('_AM_SMILEEMOTION','Beschreibung:');
+define('_AM_ADD','Hinzufügen');
+define('_AM_SAVE','Speichern');
+define('_AM_WAYSYWTDTS','WARNUNG: Sind Sie sicher, dass Sie dieses Smiley löschen möchten?');
+define('_AM_DISPLAYF', 'Im Formular anzeigen?');
+?>
