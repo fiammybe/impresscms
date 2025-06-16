8639132c6974ce264322a4db841bf5d7d622bd1d
David Janssens <fiammybe@gmail.com>
2025-06-16 12:03:40 +0200

    New translations adsense.php (German)

---
+++ htdocs/modules/system/language/de/admin/adsense.php
@@ -0,0 +1,52 @@
+<?php
+/**
+* Adsense.php definations
+*
+* @copyright      http://www.impresscms.org/ The ImpressCMS Project
+* @license         LICENSE.txt
+* @package	adsense
+* @author		marcan <marcan@impresscms.org>
+* @version		$Id$
+*/
+
+if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
+
+define('_CO_ICMS_ADSENSES', 'AdSense Anzeigen ');
+define('_CO_ICMS_ADSENSES_DSC', 'Hier finden Sie eine Liste der AdSense-Anzeigen im System.');
+define('_CO_ICMS_ADSENSE', 'AdSense Anzeigen ');
+define('_CO_ICMS_ADSENSES_CREATE', 'Erstellen Sie eine AdSense Anzeige');
+define('_CO_ICMS_ADSENSE_CREATE', 'Erstellen Sie eine AdSense Anzeige');
+define('_CO_ICMS_ADSENSES_CREATE_INFO', 'Füllen Sie einfach dieses Formular aus, um eine AdSense-Anzeige hinzuzufügen.');
+define('_CO_ICMS_ADSENSE_DELETE_CONFIRM', 'Möchten Sie diese Adsense-Anzeige wirklich aus dem System löschen?');
+define('_CO_ICMS_ADSENSES_EDIT', 'Bearbeiten einer AdSense Anzeige');
+define('_CO_ICMS_ADSENSES_EDIT_INFO', 'Verwenden Sie dieses Formular, um die Informationen dieser AdSense-Anzeige zu bearbeiten.');
+define('_CO_ICMS_ADSENSES_INFO', 'AdSense-Anzeigeninformationen ');
+define('_CO_ICMS_ADSENSE_NOT_FOUND', 'Die ausgewählte AdSense-Anzeige wurde nicht gefunden.');
+define('_CO_ICMS_ADSENSES_CREATED', 'Die AdSense-Anzeige wurde erfolgreich erstellt.');
+define('_CO_ICMS_ADSENSES_MODIFIED', 'Die AdSense-Anzeige wurde erfolgreich geändert.');
+define('_CO_ICMS_ADSENSE_TAG_CODE', 'IcmsCode');
+define('_CO_ICMS_ADSENSE_COLOR_LINK', 'Link Farbe');
+define('_CO_ICMS_ADSENSE_COLOR_LINK_DSC', '');
+define('_CO_ICMS_ADSENSE_CLIENT_ID', 'Publisher ID');
+define('_CO_ICMS_ADSENSE_CLIENT_ID_DSC', 'wie <i>ca-pub-0000000000000000</i>');
+define('_CO_ICMS_ADSENSE_SLOT', 'Platz');
+define('_CO_ICMS_ADSENSE_SLOT_DSC', '');
+define('_CO_ICMS_ADSENSE_CLONE', 'Klonen');
+define('_CO_ICMS_ADSENSE_DESCRIPTION', 'Beschreibung');
+define('_CO_ICMS_ADSENSE_DESCRIPTION_DSC', 'Beschreibung');
+define('_CO_ICMS_ADSENSE_FORMAT', 'Formate');
+define('_CO_ICMS_ADSENSE_FORMAT_DSC', '');
+define('_CO_ICMS_ADSENSE_STYLE', 'Style Informationen');
+define('_CO_ICMS_ADSENSE_STYLE_DSC', 'Die Anzeige wird in einem Container mit folgenden Stilinformationen angezeigt. Beispiel: text-align:center; padding-top: 15px;');
+define('_CO_ICMS_ADSENSE_TAG', 'ID des [adsense] Tags, um diese Anzeige anzuzeigen');
+define('_CO_ICMS_ADSENSE_TAG_DSC', 'Das Modul generiert automatisch für Sie eine ID. Wenn Sie es jedoch anpassen möchten, können Sie es tun. Denken Sie daran, dass dies einzigartig sein muss.');
+define('_CO_ICMS_ADSENSE_EXPLAIN_TITLE', 'Was sind AdSense ?');
+define('_CO_ICMS_ADSENSE_EXPLAIN', 'Adsenses sind Tags, die Sie überall auf Ihrer ImpressCMS-Website definieren und verwenden können. Sie können sie als erweiterte BBCodes sehen, aber in allen ImpressCMS Textfeldern sowie in allen Vorlagen verwendbar sein! Die vollständige Dokumentation dieser Funktion finden Sie hier: <a rel="external" href="http://www.impresscms.org/modules/simplywiki/index.php?page=Adsense">Adsenses</a>.');
+define('_CO_ICMS_ADSENSE_COLOR_BACKGROUND', 'Hintergrundfarbe');
+define('_CO_ICMS_ADSENSE_COLOR_BACKGROUND_DSC', 'Die Hintergrundfarbe Ihrer Textanzeige');
+define('_CO_ICMS_ADSENSE_COLOR_BORDER', 'Rahmenfarben');
+define('_CO_ICMS_ADSENSE_COLOR_BORDER_DSC', 'Die Rahmenfarbe Ihrer Anzeigenbereiche');
+define('_CO_ICMS_ADSENSE_COLOR_TEXT', 'Textfarbe');
+define('_CO_ICMS_ADSENSE_COLOR_TEXT_DSC', 'Die Textfarbe Ihrer Anzeigen');
+define('_CO_ICMS_ADSENSE_COLOR_URL', 'Farbe der URL');
+define('_CO_ICMS_ADSENSE_COLOR_URL_DSC', 'Die Farbe, in der die Url\'s in der Werbung angezeigt werden');
