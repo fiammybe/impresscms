5143b0b6dd5713db4e4b8b6c49a282e003cd7756
David Janssens <fiammybe@gmail.com>
2025-06-16 12:02:49 +0200

    New translations modinfo.php (German)

---
+++ htdocs/modules/system/language/de/modinfo.php
@@ -0,0 +1,72 @@
+<?php
+// $Id: modinfo.php 10326 2010-07-11 18:54:25Z malanciault $
+// Module Info
+
+// The name of this module
+define("_MI_SYSTEM_NAME","System");
+
+// A brief description of this module
+define("_MI_SYSTEM_DESC","Zur Verwaltung der zentralen Einstellungen der Website.");
+
+// Names of blocks for this module (Not all module has blocks)
+define("_MI_SYSTEM_BNAME2","Benutzermenü");
+define("_MI_SYSTEM_BNAME3","Anmelden");
+define("_MI_SYSTEM_BNAME4","Suchen");
+define("_MI_SYSTEM_BNAME5","Warte auf Inhalte");
+define("_MI_SYSTEM_BNAME6","Hauptmenü");
+define("_MI_SYSTEM_BNAME7","Webseiten-Info");
+define('_MI_SYSTEM_BNAME8', "Wer ist online");
+define('_MI_SYSTEM_BNAME9', "Top Poster");
+define('_MI_SYSTEM_BNAME10', "Neue Mitglieder");
+define('_MI_SYSTEM_BNAME11', "Neueste Kommentare");
+// RMV-NOTIFY
+define('_MI_SYSTEM_BNAME12', "Benachrichtigungsoptionen");
+define('_MI_SYSTEM_BNAME13', "Themes");
+define('_MI_SYSTEM_BNAME14', "Sprachauswahl");
+
+/**
+ * @todo Remove this blocks on future versions. When 1.1.2 isn't supported anymore.
+ */
+define('_MI_SYSTEM_BNAME15', "Inhalt");
+define('_MI_SYSTEM_BNAME16', "Inhaltsmenü");
+define('_MI_SYSTEM_BNAME17', "Ähnliche Inhalte");
+/**/
+
+define('_MI_SYSTEM_BNAME18', "Teilen Sie diese Seite!");
+
+// Names of admin menu items
+define("_MI_SYSTEM_ADMENU2","Blöcke");
+define("_MI_SYSTEM_ADMENU3","Gruppen");
+define("_MI_SYSTEM_ADMENU5","Module");
+define("_MI_SYSTEM_ADMENU6","Einstellungen");
+define("_MI_SYSTEM_ADMENU7","Smiley");
+define("_MI_SYSTEM_ADMENU9","Benutzer-Ränge");
+define("_MI_SYSTEM_ADMENU10","Benutzer bearbeiten");
+define("_MI_SYSTEM_ADMENU11","E-Mail Benutzer");
+define("_MI_SYSTEM_ADMENU12", "Benutzer finden");
+define("_MI_SYSTEM_ADMENU13", "Bilder-Manager");
+define("_MI_SYSTEM_ADMENU14", "Profilbild");
+define("_MI_SYSTEM_ADMENU15", "Templates");
+define("_MI_SYSTEM_ADMENU16", "Kommentare");
+// Version Added
+define("_MI_SYSTEM_ADMENU17", "Version");
+define("_MI_SYSTEM_ADMENU19", "Block Positionen");
+define("_MI_SYSTEM_ADMENU20", "Symlink Manager");
+define("_MI_SYSTEM_ADMENU21", "Eigene Schlagwörter");
+
+######################## Added in 1.2 ###################################
+define("_MI_SYSTEM_ADMENU22", "AdSense Anzeigen ");
+define("_MI_SYSTEM_ADMENU23", "Bewertungen");
+define("_MI_SYSTEM_ADMENU24", "MimeTypen");
+
+define('_MI_SYSTEM_BNAME101', "System-Warnungen");
+define('_MI_SYSTEM_BNAME102', "Systemsteuerung");
+define('_MI_SYSTEM_BNAME103', "Installierte Module");
+define('_MI_SYSTEM_BLOCK_BOOKMARKS','Meine Lesezeichen');
+define('_MI_SYSTEM_BLOCK_BOOKMARKS_DESC','Seiten, die ich als Lesezeichen markiert habe');
+
+define("_MI_SYSTEM_ADMENU25", "Automatische Aufgaben");
+define('_MI_SYSTEM_REMOVEUSERS','Benutzer werden deaktiviert');
+
+// Added in 1.3
+define('_MI_SYSTEM_BLOCK_CP_NEW', 'Neue Systemsteuerung');
