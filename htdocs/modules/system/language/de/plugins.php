7a21e086b032e96486b380bddb642ee6cb46ecd0
David Janssens <fiammybe@gmail.com>
2025-06-16 12:03:15 +0200

    New translations plugins.php (German)

---
+++ htdocs/modules/system/language/de/plugins.php
@@ -0,0 +1,24 @@
+<?php
+// $Id: plugins.php 8769 2009-05-15 11:12:11Z sato $
+// FILE		::	plugins.php
+// AUTHOR	::	Ryuji AMANO <info@ryus.biz>
+// WEB		::	Ryu's Planning <http://ryus.biz/>
+//
+define("_PI_WAITING_WAITINGS","Wartet");
+define("_PI_WAITING_SUBMITTED","Übermittelt");
+define("_PI_WAITING_INACTIVE_USERS","Inaktive Benutzer");
+define("_PI_WAITING_BROKENS","Defekt");
+define("_PI_WAITING_MODREQS","Auf Änderung wartend");
+define("_PI_WAITING_REQUESTS","Anfragen");
+define("_PI_WAITING_REVIEWS","Bewertungen");
+define("_PI_WAITING_PREVIEWS","Vorschau");
+define("_PI_WAITING_COMMENTS","Kommentare");
+define("_PI_WAITING_LINKS","Links");
+define("_PI_WAITING_FILES","Dateien");
+define("_PI_WAITING_BLOGS","Blogs");
+define("_PI_WAITING_EVENTS","Ereignisse");
+define("_PI_WAITING_ASKED","Angefragt");
+define("_PI_WAITING_NEWANSWERS","Neue Antworten");
+define("_PI_WAITING_ANSWERED","Beantwortet");
+define("_PI_WAITING_WAITINGS_FMT","Wartet auf (%s)");
+?>
