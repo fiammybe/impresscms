d570e46440dd5835c237ee18fd0d49a6521abd1a
David Janssens <fiammybe@gmail.com>
2025-06-16 12:12:04 +0200

    New translations userrank.php (German)

---
+++ htdocs/modules/system/language/de/admin/userrank.php
@@ -0,0 +1,32 @@
+<?php
+// $Id: userrank.php 9538 2009-11-13 18:59:32Z pesianstranger $
+
+######################## Added in 1.2 ###################################
+define('_CO_ICMS_USERRANKS','Benutzer-Ranks Einstellungen');
+define('_CO_ICMS_USERRANKS_DSC', 'Hier ist eine Liste der im System verfügbaren Benutzer-Ränge.');
+define('_CO_ICMS_USERRANK', 'Benutzer-Rang');
+define('_CO_ICMS_USERRANKS_CREATE', 'Benutzer-Rang hinzufügen');
+define('_CO_ICMS_USERRANKS_CREATE_INFO', 'Füllen Sie einfach dieses Formular aus, um einen Benutzerrang hinzuzufügen.');
+define('_CO_ICMS_USERRANKS_DELETE_CONFIRM', 'Möchten Sie wirklich diesen Benutzerrang vom System löschen ?');
+define('_CO_ICMS_USERRANKS_EDIT', 'Benutzer-Rang bearbeiten');
+define('_CO_ICMS_USERRANKS_EDIT_INFO', 'Benutzen Sie dieses Formular, um die Informationen dieses Benutzerranges zu bearbeiten.');
+define('_CO_ICMS_USERRANK_INFO', 'Benutzerrang Information');
+define('_CO_ICMS_USERRANK_NOT_FOUND', 'Der ausgewählte Benutzerrang wurde nicht gefunden.');
+
+define('_CO_ICMS_USERRANKS_CREATED', 'Der Benutzerrang wurde erfolgreich hinzugefügt.');
+define('_CO_ICMS_USERRANKS_MODIFIED', 'Der Benutzerrang wurde erfolgreich geändert.');
+
+define('_CO_ICMS_USERRANK_RANK_SPECIAL','Spezielle Ränge');
+define('_CO_ICMS_USERRANK_RANK_SPECIAL_DSC','(Besondere Ränge können Benutzern unabhängig von der Anzahl der Benutzerbeiträge zugewiesen werden)');
+define('_CO_ICMS_USERRANK_RANK_TITLE','Rang Titel');
+define('_CO_ICMS_USERRANK_RANK_TITLE_DSC','');
+define('_CO_ICMS_USERRANK_RANK_MIN','Min. Beiträge');
+define('_CO_ICMS_USERRANK_RANK_MIN_DSC', '');
+define('_CO_ICMS_USERRANK_RANK_MAX','Max. Beiträge');
+define('_CO_ICMS_USERRANK_RANK_MAX_DSC', '');
+define('_CO_ICMS_USERRANK_RANK_IMAGE','Bilder');
+define('_CO_ICMS_USERRANK_RANK_IMAGE_DSC', '');
+
+define('_CO_ICMS_USERRANK_EXPLAIN_TITLE', 'Was sind Benutzer-Ränge ?');
+define('_CO_ICMS_USERRANK_EXPLAIN', 'Benutzer Ränge sind Bild, um zwischen den Benutzern in verschiedenen Ebenen Ihrer Website zu unterscheiden! Vollständige Dokumentation dieser Funktion findest du hier: <a rel="external" href="https://www.impresscms.org/modules/simplywiki/index.php?page=User_ranks">Benutzerrang</a>.');
+?>
