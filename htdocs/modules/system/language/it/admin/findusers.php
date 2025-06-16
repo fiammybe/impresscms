fe562a27487ac8f0939af65af1f7f858b10a2e6b
David Janssens <fiammybe@gmail.com>
2025-06-16 12:06:51 +0200

    New translations findusers.php (Italian)

---
+++ htdocs/modules/system/language/it/admin/findusers.php
@@ -0,0 +1,57 @@
+<?php
+// $Id: findusers.php 11344 2011-08-30 16:33:29Z phoenyx $
+//%%%%%%	File Name findusers.php 	%%%%%
+
+define("_AM_FINDUS","Trova Utenti");
+define("_AM_AVATAR","Avatar");
+define("_AM_REALNAME","Nome");
+define("_AM_REGDATE","Data di registrazione");
+define("_AM_EMAIL","Email");
+define("_AM_PM","PM");
+define("_AM_URL","URL");
+define("_AM_PREVIOUS","<< Precedente");
+define("_AM_NEXT","Successiva >>");
+define("_AM_USERSFOUND","%s utente/i trovato/i");
+
+define("_AM_ACTUS", "Utenti attivi: %s");
+define("_AM_INACTUS", "Utenti non attivi: %s");
+define("_AM_NOFOUND","Nessun Utente trovato");
+define("_AM_UNAME","Nome Utente");
+define("_AM_ICQ","ICQ");
+define("_AM_AIM","AIM");
+define("_AM_YIM","YIM");
+define("_AM_MSNM","MSNM");
+define("_AM_LOCATION","La localit&agrave; contiene");
+define("_AM_OCCUPATION","L'occupazione contiene");
+define("_AM_INTEREST","Gli interessi contengono");
+define("_AM_URLC","L'URL contiene");
+define("_AM_LASTLOGMORE","Last login is more than <span style='color:#ff0000;'>X</span> days ago");
+define("_AM_LASTLOGLESS","Last login is less than <span style='color:#ff0000;'>X</span> days ago");
+define("_AM_REGMORE","Joined date is more than <span style='color:#ff0000;'>X</span> days ago");
+define("_AM_REGLESS","Joined date is less than <span style='color:#ff0000;'>X</span> days ago");
+define("_AM_POSTSMORE","Number of Posts is greater than <span style='color:#ff0000;'>X</span>");
+define("_AM_POSTSLESS","Number of Posts is less than <span style='color:#ff0000;'>X</span>");
+define("_AM_SORT","Ordina per");
+define("_AM_ORDER","Modalit&agrave; ordine");
+define("_AM_LASTLOGIN","Ultimo login");
+define("_AM_POSTS","Numero di messaggi");
+define("_AM_ASC","Ordine crescente");
+define("_AM_DESC","Ordine decrescente");
+define("_AM_LIMIT","Numero di utenti per pagina");
+define("_AM_RESULTS", "Risultati della ricerca");
+define("_AM_SHOWMAILOK", "Accept mail");
+define("_AM_MAILOK","Solo utenti che accettano mail");
+define("_AM_MAILNG","Solo utenti che non accettano mail");
+define("_AM_SHOWTYPE", "Attivo");
+define("_AM_ACTIVE","Only active users");
+define("_AM_INACTIVE","Only inactive users");
+define("_AM_BOTH", "All users");
+define("_AM_SENDMAIL", "Send mail");
+define("_AM_ADD2GROUP", "Add users to %s group");
+define("_AM_GROUPS", "Gruppi");
+
+######################## Added in 1.2 ###################################
+define("_AM_LOGINNAME","Nome login");
+
+######################## Added in 1.3 ###################################
+define("_AM_ACTIONS","Actions");
