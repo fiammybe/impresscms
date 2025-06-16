2c27f5bd4982170b4002c2d193ec6d5af8253034
David Janssens <fiammybe@gmail.com>
2025-06-16 12:01:39 +0200

    New translations blocks.php (Dutch)

---
+++ htdocs/modules/system/language/nl/blocks.php
@@ -0,0 +1,62 @@
+<?php
+// $Id: blocks.php 9349 2009-09-06 19:25:40Z Phoenyx $
+// Blocks
+define("_MB_SYSTEM_ADMENU","Administratie Menu");
+define("_MB_SYSTEM_RNOW","Registreer Nu!");
+define("_MB_SYSTEM_LPASS","Paswoord verloren?");
+define("_MB_SYSTEM_SEARCH","Zoeken");
+define("_MB_SYSTEM_ADVS","Geavanceerd zoeken");
+define("_MB_SYSTEM_VACNT","Bekijk Gebruikersprofiel");
+define("_MB_SYSTEM_EACNT","Bewerk Gebruikersprofiel");
+// RMV-NOTIFY
+define("_MB_SYSTEM_NOTIF", "Meldingen");
+define("_MB_SYSTEM_LOUT","Uitloggen");
+define("_MB_SYSTEM_INBOX","Postvak in");
+define("_MB_SYSTEM_HOME","Startpagina"); // link to home page in main menu block
+define("_MB_SYSTEM_RECO","Raad ons aan");
+define("_MB_SYSTEM_PWWIDTH","Pop-Up venster breedte");
+define("_MB_SYSTEM_PWHEIGHT","Pop-Up venster hoogte");
+define("_MB_SYSTEM_LOGO","Logo afbeelding in de %s map");  // %s is your root image directory name
+
+define("_MB_SYSTEM_SADMIN","Toon administrator groepen");
+
+define("_MB_SYSTEM_DISPLAY","Toon %s leden");
+define("_MB_SYSTEM_DISPLAYA","Toon leden avatars");
+define("_MB_SYSTEM_NODISPGR","Toon geen leden met de status:");
+
+define("_MB_SYSTEM_DISPLAYC","Toon %s commentaren");
+define("_MB_SYSTEM_SECURE", "Veilige login");
+
+define("_MB_SYSTEM_NUMTHEME", "%s themes");
+define("_MB_SYSTEM_THSHOW", "Toon screenshot beeld");
+define("_MB_SYSTEM_THWIDTH", "Screenshot afbeelding breedte");
+define('_MB_SYSTEM_REMEMBERME', 'Herinner me');
+define("_MB_SYSTEM_PRIVPOLICY", "Privacy verklaring");
+
+/*
+ * Added in 1.2
+ */
+
+define("_MB_SYSTEM_TOTAL_USERS", "Totaal aantal gebruikers");
+define("_MB_SYSTEM_ACT_USERS", "Actieve gebruikers");
+define("_MB_SYSTEM_INACT_USERS", "Inactieve gebruikers");
+define("_MB_SYSTEM_DISPLAYTOT","Toon totaal aantal leden?");
+// waiting content
+define("_MB_SYSTEM_NOWAITING_DISPLAY","Altijd getoond");
+define("_MB_SYSTEM_SQL_CACHE","SQL cache");
+// Social bookmarking
+define("_MB_SYSTEM_SOCIAL_PROVIDER_SELECT", "Selecteer weer te geven social bookmark providers");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_BOOKMARK", "bookmark at: ");
+
+/*
+ * Updated in 2.0 RC2
+ */
+
+define("_MB_SYSTEM_SOCIAL_PROVIDER_0", "Twitter");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_1", "Facebook");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_2", "Delicious");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_3", "Ask");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_4", "Digg");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_5", "Reddit");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_6", "Yahoo");
+define("_MB_SYSTEM_SOCIAL_PROVIDER_7", "Diigo");
