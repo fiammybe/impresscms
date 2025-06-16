12a04e8e550e8bb07a10ac474800a6ee2a334050
David Janssens <fiammybe@gmail.com>
2025-06-16 12:05:53 +0200

    New translations comments.php (German)

---
+++ htdocs/modules/system/language/de/admin/comments.php
@@ -0,0 +1,14 @@
+<?php
+// $Id: comments.php 11344 2011-08-30 16:33:29Z phoenyx $
+//%%%%%% Comment Manager %%%%%
+define('_MD_AM_COMMMAN','Kommentar Manager');
+
+define('_MD_AM_LISTCOMM','Kommentarliste');
+define('_MD_AM_ALLMODS','Alle Module');
+define('_MD_AM_ALLSTATUS','Beliebiger Status');
+define('_MD_AM_MODULE','Module');
+define('_MD_AM_COMFOUND','%s Kommentar(e) gefunden.');
+
+// Added in 1.3
+define('_MD_AM_ACTION', 'Aktion');
+define('_MD_AM_MESSAGE_ICON', 'Icon');
