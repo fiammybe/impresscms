8b913026d3dc5f4aa9650c2ad6d78660c52e3c6d
David Janssens <fiammybe@gmail.com>
2025-06-16 12:10:56 +0200

    New translations rating.php (Dutch)

---
+++ htdocs/modules/system/language/nl/admin/rating.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * rating definations
+ *
+ * @copyright http://www.impresscms.org/ The ImpressCMS Project
+ * @license LICENSE.txt
+ * @package ratings
+ * @author marcan <macrcan@impresscms.org>
+ */
+if (!defined('ICMS_ROOT_PATH')) die("ImpressCMS root path not defined");
+
+define('_CO_ICMS_RATING_RATE_IT', 'Beoordelen !');
+define('_CO_ICMS_RATING_DUPLICATE_ENTRY', 'Excuses! U heeft dit al beoordeeld! U kunt iets niet tweemaal beoordelen.');
+define('_CO_ICMS_RATING_ERROR', 'An error occurred, your rating could not be stored.');
+define('_CO_ICMS_RATING_SUCCESS', 'Uw beoordeling is opgeslagen. Dank u voor uw bijdrage!');
+define('_CO_ICMS_RATING_RATE_THIS', 'Was this useful ? (1 being <i>Not useful</i> and 5 being <i>Very useful</i>)');
+define('_CO_ICMS_RATING_ALREADY_RATED', 'Uw beoordeling: ');
+define('_CO_ICMS_RATING_VOTERS_TOTAL', 'Aantal stemmers: ');
+define('_CO_ICMS_RATING_AVERAGE', 'Gemiddeld: ');
+define('_CO_ICMS_RATINGS', 'Beoordeling');
+define('_CO_ICMS_RATINGS_DSC', 'Hier is een overzicht van toegevoegde beoordelingen in het systeem.');
+define('_CO_ICMS_RATING', 'Beoordeling');
+define('_CO_ICMS_RATINGS_CREATE', 'Beoordeling toevoegen');
+define('_CO_ICMS_RATING_CREATE', 'Beoordeling toevoegen');
+define('_CO_ICMS_RATINGS_CREATE_INFO', 'Vult dit formulier in om een beoordeling toe te voegen.');
+define('_CO_ICMS_RATING_DELETE_CONFIRM', 'Wilt u deze beoordeling verwijderen van het systeem?');
+define('_CO_ICMS_RATINGS_EDIT', 'Beoordeling aanpassen');
+define('_CO_ICMS_RATINGS_EDIT_INFO', 'Gebruik dit formulier om de informatie van deze beoordeling aan te passen.');
+define('_CO_ICMS_RATINGS_INFO', 'Beoordelingsinformatie');
+define('_CO_ICMS_RATING_NOT_FOUND', 'De geselecteerde beoordeling is niet gevonden.');
+define('_CO_ICMS_RATINGS_CREATED', 'De beoordeling is succesvol aangemaakt.');
+define('_CO_ICMS_RATINGS_MODIFIED', 'De beoordeling is succesvol aangepast.');
+define('_CO_ICMS_RATING_EXPLAIN_TITLE', 'Wat zijn beoordelingen ?');
+define('_CO_ICMS_RATING_EXPLAIN', 'Ratings is a new feature being added ImpressCMS. With using this tool, you will be able to add a new rating method to your modules, and control the results through this section! Documentation for this feature can be found here: <a rel="external" href="https://www.impresscms.org/modules/simplywiki/index.php?page=Ratings">Rating Features</a>. Let us know how you would like to see this developed');
