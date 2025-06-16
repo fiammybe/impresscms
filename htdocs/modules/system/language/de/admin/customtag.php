0c1e496b0e3cac3e13b0d7296ec66d1c0b0f8183
David Janssens <fiammybe@gmail.com>
2025-06-16 12:06:19 +0200

    New translations customtag.php (German)

---
+++ htdocs/modules/system/language/de/admin/customtag.php
@@ -0,0 +1,37 @@
+<?php
+
+define('_CO_ICMS_CUSTOMTAGS','Eigene Schlagwörter');
+define('_CO_ICMS_CUSTOMTAGS_DSC', 'Hier ist eine Liste der benutzerdefinierten Schlagwörter, die im System verfügbar sind.');
+define('_CO_ICMS_CUSTOMTAG', 'Eigene Schlagwörter');
+define('_CO_ICMS_CUSTOMTAG_CREATE', 'Benutzerdefiniertes Schlagwörter erstellen');
+define('_CO_ICMS_CUSTOMTAG_CREATE_INFO', 'Füllen Sie einfach dieses Formular aus, um ein eigenes Schlagwörter hinzuzufügen.');
+define('_CO_ICMS_CUSTOMTAG_DELETE_CONFIRM', 'Möchten Sie wirklich dieses benutzerdefinierte Schlagwort aus dem System löschen ?');
+define('_CO_ICMS_CUSTOMTAG_EDIT', 'Benutzerdefiniertes Schlagwort bearbeiten');
+define('_CO_ICMS_CUSTOMTAG_EDIT_INFO', 'Verwenden Sie dieses Formular, um die Informationen dieses benutzerdefinierten Tags zu bearbeiten.');
+define('_CO_ICMS_CUSTOMTAG_INFO', 'Eigene Schlagwortinformation');
+define('_CO_ICMS_CUSTOMTAG_NOT_FOUND', 'Das ausgewählte benutzerdefinierte Schlagwort wurde nicht gefunden.');
+
+define('_CO_ICMS_CUSTOMTAG_CREATED', 'Das benutzerdefinierte Schlagwort wurde erfolgreich erstellt.');
+define('_CO_ICMS_CUSTOMTAG_MODIFIED', 'Das eigene Tag wurde erfolgreich geändert.');
+
+define('_CO_ICMS_CUSTOMTAG_NAME', 'Name');
+define('_CO_ICMS_CUSTOMTAG_NAME_DSC', '');
+define('_CO_ICMS_CUSTOMTAG_DESCRIPTION', 'Beschreibung');
+define('_CO_ICMS_CUSTOMTAG_DESCRIPTION_DSC', '');
+define('_CO_ICMS_CUSTOMTAG_CONTENT', 'Inhalt');
+define('_CO_ICMS_CUSTOMTAG_CONTENT_DSC', '');
+define('_CO_ICMS_CUSTOMTAG_LANGUAGE', 'Sprache');
+define('_CO_ICMS_CUSTOMTAG_LANGUAGE_DSC', '');
+define('_CO_ICMS_CUSTOMTAG_TYPE', 'Typ');
+define('_CO_ICMS_CUSTOMTAG_TYPE_DSC', '');
+
+define('_CO_ICMS_CUSTOMTAG_PERMISSION_VIEW', 'Berechtigung anzeigen');
+define('_CO_ICMS_CUSTOMTAG_PERMISSION_VIEW_DSC', 'Nur die ausgewählten Gruppen können den Inhalt dieses Tags sehen');
+
+define('_CO_ICMS_CUSTOMTAG_CLONE', 'Klonen');
+
+define('_CO_ICMS_CUSTOMTAGS_EXPLAIN_TITLE', 'Was sind benutzerdefinierte Schlagworte?');
+define('_CO_ICMS_CUSTOMTAGS_EXPLAIN', 'Benutzerdefinierte Tags sind Tags, die Sie überall auf Ihrer ImpressCMS-Website definieren und verwenden können. Sie könnten sie als erweiterte BBCodes, aber mehrsprachig, mit Gruppenberechtigungen, betrachten Unterstützung von PHP und verwendbar in allen ImpressCMS Textfeldern sowie in allen Vorlagen ! Vollständige Dokumentation dieser Funktion finden Sie hier: <a rel="external" href="https://www.impresscms.org/modules/simplywiki/index.php?page=Custom_Tags">Benutzerdefinierte Tags</a>.');
+######################## Added in 1.2 ###################################
+define('_CO_ICMS_CUSTOMTAGS_TAG_CODE', 'IcmsCode');
+?>
