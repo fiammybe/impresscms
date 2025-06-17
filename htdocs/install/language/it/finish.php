afbdda5778145f0b82ecfdfd21fdca8b4d99a011
David Janssens <fiammybe@gmail.com>
2025-06-17 12:01:54 +0200

    New translations finish.php (Italian)

--- htdocs/install/language/it/finish.php
+++ htdocs/install/language/it/finish.php
@@ -3,15 +3,13 @@
 $content .=
 "<h3>Support</h3>
 <ul class=\"diags\">
-<li><a href='https://www.impresscms.org/' target='_blank'>Our Home - Project Homepage</a></li>" .
-"<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>Community Forums - Support</a></li>" .
+<li><a href='https://www.impresscms.org/' target='_blank'>La Nostra Casa - Homepage Del Progetto</a></li>" .
+"Forum Comunitari <li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>- Supporto</a></li>" .
 "<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>Downloads - Modules & Themes</a></li>" .
-"<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Community Documentation</a></li>".
-"</ul>
+"<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Documentazione Comunitaria</a></li>".
+"<h3>Iscriviti alla newsletter</h3>
+<p>Ricordati di iscriverti alla <a href='http://eepurl.com/dqXVIb' target='_blank'>ImpressCMS Newsletter</a>per novità di nuovi moduli, temi, aggiornamenti importanti e altro ancora!</p>
 
-<h3>Subscribe to the newsletter</h3>
-<p>Please remember to subscribe to the <a href='http://eepurl.com/dqXVIb' target='_blank'>ImpressCMS Newsletter</a>, for news of new modules, themes, important updates and more!</p>
-
-<h3>Your New Site</h3>
-<p>You can now access the home page of your site, by clicking on the button below and finishing the wizard.</p>
+<h3>Il tuo nuovo sito</h3>
+<p>Ora puoi accedere alla home page del tuo sito, cliccando sul pulsante qui sotto e terminando la procedura guidata.</p>
 ";
