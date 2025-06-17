9cc4fbb8096e7ecdd8879d2df98f6cc56361343c
David Janssens <fiammybe@gmail.com>
2025-06-17 12:02:57 +0200

    New translations welcome.php (Italian)

--- htdocs/install/language/it/welcome.php
+++ htdocs/install/language/it/welcome.php
@@ -2,28 +2,28 @@
 // $Id: welcome.php 12227 2013-07-19 08:07:21Z fiammy $
 $content .= '
 <p>
-	ImpressCMS is a Web publishing system written in PHP. It is an ideal tool for
-	developing small to large dynamic community websites, intra company portals, corporate portals, weblogs and much more.
+	<unk> ImpressCMS è un sistema di pubblicazione Web scritto in PHP. È uno strumento ideale per
+	<unk> sviluppando siti web di comunità dinamici di piccole o grandi dimensioni, portali aziendali, weblogs e molto altro ancora.
 </p>
 <p>
-	ImpressCMS is released under the terms of the
-	<a href="https://www.gnu.org/copyleft/gpl.html" rel="external">GNU General Public License (GPL)</a>
-	and is free to use and modify.
-	It is free to redistribute as long as you abide by the distribution terms of the GPL.
+	<unk> ImpressCMS è rilasciato secondo i termini della
+	<a href="http://www.gnu.org/copyleft/gpl.html" rel="external">GNU General Public License (GPL)</a>
+	<unk> ed è libero di usare e modificare.
+	<unk> È libero di ridistribuire fino a quando si rispettano i termini di distribuzione della GPL.
 </p>
-<h3>Requirements</h3>
+<h3>Requisiti</h3>
 <ul>
-	<li>- WWW Server: <a href="https://www.apache.org/" rel="external">Apache</a>, NGinx, IIS, Roxen, etc</li>
-	<li>- Script: <a href="https://www.php.net/" rel="external">PHP</a> 7.4+ and 16mb minimum memory allocation - PHP 8.1 is recommended!</li>
-	<li>- Database: <a href="https://www.mysql.com/" rel="external">MySQL</a> 4.1.0 or higher, <a href="https://mariadb.org/" rel="external">MariaDB</a> 5.1 or higher</li>
+	<li>- WWW Server: <a href="http://www.apache.org/" rel="external">Apache</a>NGinx, IIS, Roxen, etc</li>
+	<li>- Script: <a href="http://www.php.net/" rel="external">PHP</a> 7.4+ e allocazione di memoria minima 32mb - PHP 8.3 è altamente raccomandato!</li>
+	<li>- Database: <a href="http://www.mysql.com/" rel="external">MySQL</a> 4.1 o superiore, <a href="https://mariadb.org/" rel="external">MariaDB</a> 5.2 o superiore</li>
 </ul>
-<h3>Before you install</h3>
+<h3>Prima di installare</h3>
 <ul>
-	<li>Setup the web server, PHP and database server properly.</li>
-	<li>Prepare a database for ImpressCMS. This can be an existing database as well as a newly created one.</li>
-	<li>Prepare a user account and grant this user access to the database (all rights).</li>
-	<li>Make the directories of uploads/, cache/, templates_c/, modules/ writable (chmod 777 or 755 - depending on servers).</li>
-	<li>Make the file mainfile.php writable (chmod 666 depending on server).</li>
-	<li>In your internet browser settings turn on the allowance of cookies and JavaScript.</li>
+	<li>Impostare il server web, PHP e server database correttamente.</li>
+	<li>Prepara un database per ImpressCMS. Può trattarsi di un database esistente e di un nuovo database.</li>
+	<li>Preparare un account utente e concedere a questo utente l\'accesso al database (tutti i diritti).</li>
+	<li>Rendi le directory di uploads/, cache/, templates_c/, modules/ writable (chmod 777 o 755 - a seconda dei server).</li>
+	<li>Crea il file principale. hp scrivibile (chmod 666 a seconda del server).</li>
+	<li>Nelle impostazioni del browser internet attivare la tolleranza di cookie e JavaScript.</li>
 </ul>
 ';
