d52804bc9f58943d2e22dcd9654d4035c2c6508a
David Janssens <fiammybe@gmail.com>
2025-06-17 12:01:57 +0200

    New translations finish.php (Dutch)

--- htdocs/install/language/nl/finish.php
+++ htdocs/install/language/nl/finish.php
@@ -1,17 +1,15 @@
 <?php
 // $Id: finish.php 12168 2013-05-22 13:25:59Z skenow $
 $content .=
-"<h3>Support</h3>
+"<h3>Ondersteuning</h3>
 <ul class=\"diags\">
-<li><a href='https://www.impresscms.org/' target='_blank'>Our Home - Project Homepage</a></li>" .
-"<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>Community Forums - Support</a></li>" .
-"<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>Downloads - Modules & Themes</a></li>" .
-"<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Community Documentation</a></li>".
+<li><a href='https://www.impresscms.org/' target='_blank'>Home - Project startpagina</a></li>" .
+"<li><a href='https://www.impresscms.org/modules/iforum/' target='_blank'>Community Forums - Ondersteuning</a></li>" .
+"<li><a href='https://www.impresscms.org/modules/downloads/' target='_blank'>Downloads - Modules & Thema's</a></li>" .
+"<li><a href='https://www.impresscms.org/modules/simplywiki/' target='_blank'>Wiki - Community Documentatie</a></li>".
 "</ul>
+<h3> Abonneer u op de nieuwsbrief</h3>
+<p>Vergeet niet om u te abonneeren op de nieuwsbrief via <a href='http://eepurl.com/dqXVIb' target='_blank'>dit formulier voor nieuws over modules ,thema's, belangrijke updates en meer!</p>
 
-<h3>Subscribe to the newsletter</h3>
-<p>Please remember to subscribe to the <a href='http://eepurl.com/dqXVIb' target='_blank'>ImpressCMS Newsletter</a>, for news of new modules, themes, important updates and more!</p>
-
-<h3>Your New Site</h3>
-<p>You can now access the home page of your site, by clicking on the button below and finishing the wizard.</p>
-";
+<h3>Uw nieuwe website.</h3>
+<p>De startpagina van uw nieuws site is nu bereikbaar, door te klikken op onderstaande knop om de wizard af te sluiten.</p>";
