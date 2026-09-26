bc277c8d8f025fb5fd0f9302e3576c56bca6a5ad
David Janssens <fiammybe@gmail.com>
2026-09-26 10:56:33 +0200

    add extra object types

--- htdocs/libraries/Icms/Ipf/Handler.php
+++ htdocs/libraries/Icms/Ipf/Handler.php
@@ -785,8 +785,8 @@ class Handler extends \Icms\Core\EntityHandler {
 		}
 		$fieldsToStoreInDB = array();
 		foreach ($obj->cleanVars as $k => $v) {
-			// integer columns: also the timestamp types, so that an empty value is stored as 0 (strict SQL mode rejects '')
-			if (in_array($obj->vars[$k]['data_type'], array(XOBJ_DTYPE_INT, XOBJ_DTYPE_STIME, XOBJ_DTYPE_MTIME, XOBJ_DTYPE_LTIME, XOBJ_DTYPE_TIME_ONLY))) {
+			// integer columns: also the timestamp and url/file link types (stored as ids), so that an empty value is stored as 0 (strict SQL mode rejects '')
+			if (in_array($obj->vars[$k]['data_type'], array(XOBJ_DTYPE_INT, XOBJ_DTYPE_STIME, XOBJ_DTYPE_MTIME, XOBJ_DTYPE_LTIME, XOBJ_DTYPE_TIME_ONLY, XOBJ_DTYPE_URLLINK, XOBJ_DTYPE_FILE))) {
 				$cleanvars[$k] = (int) ($v);
 			} elseif (is_array($v)) {
 				$cleanvars[ $k ] = $this->db->quoteString( implode( ',', $v ) );
